ee6ad7a17a41a50ff44652db9ae5bdb72c7c3f13
Nguyễn Tiến Hùng <ngtienhung0501@gmail.com>
2022-11-28 16:19:17 +0700

    Fix

--- Assets/2_Scripts/Manager/ObjectPooler.cs
+++ Assets/2_Scripts/Manager/ObjectPooler.cs
@@ -1,5 +1,26 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+
+public enum ObjectTag
+{
+    Ball = 0,
+    Basket,
+    TrajectoryDot,
+    Star
+}
+
+[Serializable]
+public class Pool
+{
+    public GameObject prefab;
+    public ObjectTag objectTag;
+    public int poolSize;
+    public bool expandable;
+
+    [HideInInspector]
+    public List<GameObject> listObject;
+}
 
 public class ObjectPooler : MonoBehaviour
 {
